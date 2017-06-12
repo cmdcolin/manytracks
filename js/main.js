fdcf047190ba5e81f1758a215761f0247f6f6c3e
Colin <colin.diesh@gmail.com>
2017-06-12 17:25:49 -0400

    Fix eslint warnings

--- js/main.js
+++ js/main.js
@@ -13,13 +13,13 @@ function (
             var browser = this.browser = args.browser;
             var thisB = this;
 
-            browser.afterMilestone('completely initialized', function() {
-                Object.keys(browser.config.manytracks).forEach(function(key) {
+            browser.afterMilestone('completely initialized', function () {
+                Object.keys(browser.config.manytracks).forEach(function (key) {
                     var many = browser.config.manytracks[key];
-                    for(var j = 0; j < many.urlTemplates.length; j++) {
+                    for (var j = 0; j < many.urlTemplates.length; j++) {
                         thisB.createManyTracks(lang.mixin(lang.clone(many), {
                             urlTemplate: many.urlTemplates[j],
-                            label: key+'_'+many.urlTemplates[j]
+                            label: key + '_' + many.urlTemplates[j]
                         }));
                     }
                 });
@@ -28,23 +28,23 @@ function (
             console.log('ManyTracks plugin starting');
         },
 
-        createManyTracks: function(conf) {
+        createManyTracks: function (conf) {
             var browser = this.browser;
             var storeConf = lang.mixin(lang.clone(conf), {
                 browser: browser,
                 refSeq: browser.refSeq,
                 type: conf.storeClass,
-                baseUrl: browser.config.baseUrl+browser.config.dataRoot+'/'
+                baseUrl: browser.config.baseUrl + browser.config.dataRoot + '/'
             });
 
-            var storeName = browser.addStoreConfig(undefined, storeConf);
+            var storeName = browser.addStoreConfig(null, storeConf);
             storeConf.name = storeName;
-            browser.getStore(storeName, function(store) {
+            browser.getStore(storeName, function (/* store*/) {
                 var trackConfig = lang.mixin(conf, {
                     store: storeName
                 });
                 // send out a message about how the user wants to create the new tracks
-                browser.publish( '/jbrowse/v1/v/tracks/new', [trackConfig] );
+                browser.publish('/jbrowse/v1/v/tracks/new', [trackConfig]);
             });
         }
     });
