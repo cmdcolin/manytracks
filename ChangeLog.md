b0d56d39b223a415b29b8c3824441631862ff51c
Colin <colin.diesh@gmail.com>
2017-06-13 10:27:08 -0400

    Update loading mechanism

---
+++ ChangeLog.md
@@ -0,0 +1,7 @@
+# Version 0.2.0
+
+- Change loading strategy to hook into config loader instead of using the publish mechanism
+
+# Version 0.1.0
+
+- Initial release
--- js/main.js
+++ js/main.js
@@ -1,51 +1,41 @@
 define([
     'dojo/_base/declare',
     'dojo/_base/lang',
-    'JBrowse/Plugin'
+    'JBrowse/Plugin',
+    'JBrowse/Digest/Crc32',
+    'JBrowse/ConfigAdaptor/JB_json_v1'
 ],
 function (
     declare,
     lang,
-    JBrowsePlugin
+    JBrowsePlugin,
+    digest,
+    Config
 ) {
     return declare(JBrowsePlugin, {
         constructor: function (args) {
             var browser = this.browser = args.browser;
             var thisB = this;
-
-            browser.afterMilestone('completely initialized', function () {
-                Object.keys(browser.config.manytracks).forEach(function (key) {
-                    var many = browser.config.manytracks[key];
-                    for (var j = 0; j < many.urlTemplates.length; j++) {
-                        thisB.createManyTracks(lang.mixin(lang.clone(many), {
-                            urlTemplate: many.urlTemplates[j],
-                            label: key + '_' + many.urlTemplates[j]
-                        }));
-                    }
-                });
+            var conf = {
+                tracks: []
+            };
+            Object.keys(browser.config.manytracks).forEach(function (key) {
+                var many = browser.config.manytracks[key];
+                for (var j = 0; j < many.urlTemplates.length; j++) {
+                    conf.tracks.push(lang.mixin(lang.clone(many), {
+                        urlTemplate: many.urlTemplates[j],
+                        urlTemplates: null,
+                        baseUrl: browser.config.baseUrl + browser.config.dataRoot + '/',
+                        label: key + '_' + many.urlTemplates[j]
+                    }));
+                }
             });
+            var adapter = new Config()
+            var newconf =  adapter.regularizeTrackConfigs(conf)
+            lang.mixin(browser.config.stores, newconf.stores)
+            browser.config.tracks = browser.config.tracks.concat(newconf.tracks)
 
             console.log('ManyTracks plugin starting');
-        },
-
-        createManyTracks: function (conf) {
-            var browser = this.browser;
-            var storeConf = lang.mixin(lang.clone(conf), {
-                browser: browser,
-                refSeq: browser.refSeq,
-                type: conf.storeClass,
-                baseUrl: browser.config.baseUrl + browser.config.dataRoot + '/'
-            });
-
-            var storeName = browser.addStoreConfig(null, storeConf);
-            storeConf.name = storeName;
-            browser.getStore(storeName, function (/* store*/) {
-                var trackConfig = lang.mixin(conf, {
-                    store: storeName
-                });
-                // send out a message about how the user wants to create the new tracks
-                browser.publish('/jbrowse/v1/v/tracks/new', [trackConfig]);
-            });
         }
     });
 });
